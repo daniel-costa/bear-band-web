ac61c0fa0418dbe69277d48beacbc910fe44926c
Julian <julianmklinger@gmail.com>
2018-02-19 00:20:15 +0100

    Corrected code, first steps JS

---
+++ assets/javascripts/main.js
@@ -0,0 +1,27 @@
+function greet(firstname) {
+  if(firstname == "Daniel") {
+    console.log("Hello " + firstname + " my Portuguese Friend");
+  } else {
+    console.log("Hello " + firstname);
+  }
+}
+
+function init() {
+  console.log("Ready");
+  $(".nav-link").on("click", test)
+}
+
+var test = function(event) {
+  dog = event.currentTarget
+  house = $(dog)
+  console.log(house.text())
+  console.log($(event.currentTarget).text())
+  // if, switch, for, while, else, else if
+}
+
+$(document).ready(init);
+
+console.log("main.js loaded");
+
+greet("Julian")
+greet("Daniel")
