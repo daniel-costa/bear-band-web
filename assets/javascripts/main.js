695f0e99d38563765148c2262867457ddc5805bb
Julian <julianmklinger@gmail.com>
2018-02-20 11:12:44 +0100

    Added responsive.css
    - commented JS
    - ordered code in style.css

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,27 +1,27 @@
-function greet(firstname) {
-  if(firstname == "Daniel") {
-    console.log("Hello " + firstname + " my Portuguese Friend");
-  } else {
-    console.log("Hello " + firstname);
-  }
-}
+// function greet(firstname) {
+//   if(firstname == "Daniel") {
+//     console.log("Hello " + firstname + " my Portuguese Friend");
+//   } else {
+//     console.log("Hello " + firstname);
+//   }
+// }
 
-function init() {
-  console.log("Ready");
-  $(".nav-link").on("click", test)
-}
+// function init() {
+//   console.log("Ready");
+//   $(".nav-link").on("click", test)
+// }
 
-var test = function(event) {
-  dog = event.currentTarget
-  house = $(dog)
-  console.log(house.text())
-  console.log($(event.currentTarget).text())
-  // if, switch, for, while, else, else if
-}
+// var test = function(event) {
+//   dog = event.currentTarget
+//   house = $(dog)
+//   console.log(house.text())
+//   console.log($(event.currentTarget).text())
+//   // if, switch, for, while, else, else if
+// }
 
-$(document).ready(init);
+// $(document).ready(init);
 
-console.log("main.js loaded");
+// console.log("main.js loaded");
 
-greet("Julian")
-greet("Daniel")
+// greet("Julian")
+// greet("Daniel")
