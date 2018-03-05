a4a072ed22e538c47f37ddf1ff45e6fd32d9e4c6
Julian <julianmklinger@gmail.com>
2018-03-05 13:21:23 +0100

    Implemented collapse navbar after click

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,31 +1,3 @@
 $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
-
-// function greet(firstname) {
-//   if(firstname == "Daniel") {
-//     console.log("Hello " + firstname + " my Portuguese Friend");
-//   } else {
-//     console.log("Hello " + firstname);
-//   }
-// }
-
-// function init() {
-//   console.log("Ready");
-//   $(".nav-link").on("click", test)
-// }
-
-// var test = function(event) {
-//   dog = event.currentTarget
-//   house = $(dog)
-//   console.log(house.text())
-//   console.log($(event.currentTarget).text())
-//   // if, switch, for, while, else, else if
-// }
-
-// $(document).ready(init);
-
-// console.log("main.js loaded");
-
-// greet("Julian")
-// greet("Daniel")
